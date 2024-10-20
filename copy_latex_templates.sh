2d8192e0c901fd62bbba389cdd195207f29453ad
thhaase <thhaase.soz@gmail.com>
2024-10-20 09:29:21 +0200

    rename .tex/.pdf; include csquotes for easier german quotation

--- copy_latex_templates.sh
+++ copy_latex_templates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SOURCE_DIR="~/Insync/thhaase.soz@gmail.com/GoogleDrive/Latex"
+SOURCE_DIR="/home/thomas/Insync/thhaase.soz@gmail.com/GoogleDrive/Latex"
 
 SOURCE_DIR=$(eval echo $SOURCE_DIR)
 
@@ -8,8 +8,8 @@ TARGET_DIR="latex"
 
 mkdir -p "$TARGET_DIR"
 
-cp -a "$SOURCE_DIR/Latex_template.tex" "$TARGET_DIR"
-cp -a "$SOURCE_DIR/Latex_template.pdf" "$TARGET_DIR"
+cp -a "$SOURCE_DIR/main.tex" "$TARGET_DIR"
+cp -a "$SOURCE_DIR/main.pdf" "$TARGET_DIR"
 
 original_target=$(readlink -f "$SOURCE_DIR/Bibliothek.bib")
 ln -s "$original_target" "$TARGET_DIR/Bibliothek.bib"
